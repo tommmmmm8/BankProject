7e9126b8392193bcc1f7460b15f4893849d1a5ca
Tom <tom8.chr@gmail.com>
2025-10-02 14:11:27 +0200

    Add JSON serialization service for BankAccountOwner and related classes

--- src/main/java/com/bank/serialization/BankAccountOwnerJsonSerializationService.java
+++ src/main/java/com/bank/serialization/BankAccountOwnerJsonSerializationService.java
@@ -4,10 +4,15 @@ import com.bank.people.BasePerson;
 import com.bank.people.customers.BankAccountOwner;
 import com.bank.people.serialization.BankAccountOwnerSerialization;
 import com.bank.people.serialization.BankAccountSerializationFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.gson.Gson;
 
 public class BankAccountOwnerJsonSerializationService implements Serialization {
 
     BankAccountSerializationFactory bankAccountSerializationFactory = new BankAccountSerializationFactory();
+    Gson gson = new Gson();
+
 
     @Override
     public String serialize(Object bankAccountOwner) {
@@ -17,23 +22,33 @@ public class BankAccountOwnerJsonSerializationService implements Serialization {
 
         BankAccountOwnerSerialization bankAccountOwnerSerialization = bankAccountSerializationFactory.createBankAccountOwnerSerialization((BankAccountOwner) bankAccountOwner);
 
-        StringBuilder builder = new StringBuilder();
-        builder.append("{");
-        builder.append("\"uuid\": " + bankAccountOwnerSerialization.uuid);
-        builder.append(", \"firstName\": \"" + bankAccountOwnerSerialization.firstName + "\"");
-        builder.append(", \"lastName\": \"" + bankAccountOwnerSerialization.lastName + "\"");
-        builder.append("}");
-
-        return builder.toString();
+        return gson.toJson(bankAccountOwnerSerialization);
     }
 
+    public String serializeXml(Object bankAccountOwner) {
+        if (!(bankAccountOwner instanceof BankAccountOwner)) {
+            throw new IllegalArgumentException("BankAccountOwner must be of type BankAccountOwner");
+        }
+
+        BankAccountOwnerSerialization bankAccountOwnerSerialization = bankAccountSerializationFactory.createBankAccountOwnerSerialization((BankAccountOwner) bankAccountOwner);
+
+        XmlMapper xmlMapper = new XmlMapper();
+        try {
+            return xmlMapper.writeValueAsString(bankAccountOwnerSerialization);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
     @Override
-    public Object deserialize(String object) {
-        return null;
+    public Object deserialize(String serializedData) {
+        return gson.fromJson(serializedData, BankAccountOwnerSerialization.class);
     }
 
     public static void main(String[] args) {
         BankAccountOwnerJsonSerializationService service = new BankAccountOwnerJsonSerializationService();
-        System.out.println(service.serialize(new BankAccountOwner("101", new BasePerson("100", "Tom", "Chrtek", 1, "man", "123 Main street"))));
+        System.out.println(service.serializeXml(new BankAccountOwner("101", new BasePerson("100", "Tom", "Chrtek", 1, "man", "123 Main street"))));
     }
 }
